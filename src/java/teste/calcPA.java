d28be283e17001b90869799c9209d977ea81e036
Alex <alexsouza8045@gmail.com>
2018-06-19 02:03:41 -0300

    paranoia

---
+++ src/java/teste/calcPA.java
@@ -0,0 +1,15 @@
+package java.teste;
+
+public class calcPA {
+
+    // Calculando um valor de uma PA - Progressa Aritmetica
+    // Atividade:  
+
+    public static void main(String[] args) {
+         
+     int n1 = 0;
+
+     System.out.println(n1);
+
+    }    
+}
